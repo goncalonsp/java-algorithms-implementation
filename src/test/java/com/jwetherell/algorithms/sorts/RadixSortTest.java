6182e7d68be49482488840973c08f6244eefff86
Gonçalo Pereira <goncalonsp@gmail.com>
2024-05-06 10:13:37 +0100

    Organize project source files. Also cleanup old tests

---
+++ src/test/java/com/jwetherell/algorithms/sorts/RadixSortTest.java
@@ -0,0 +1,13 @@
+package com.jwetherell.algorithms.sorts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadixSortTest {
+
+    @Test
+    void sort() {
+        assertTrue(true);
+    }
+}
