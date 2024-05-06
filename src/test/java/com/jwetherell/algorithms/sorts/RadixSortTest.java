616b247b27211e69543c7743126f73fdf4929efc
Gonçalo Pereira <goncalonsp@gmail.com>
2024-05-06 10:32:14 +0100

    Example test

--- src/test/java/com/jwetherell/algorithms/sorts/RadixSortTest.java
+++ src/test/java/com/jwetherell/algorithms/sorts/RadixSortTest.java
@@ -2,12 +2,17 @@ package com.jwetherell.algorithms.sorts;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadixSortTest {
 
     @Test
     void sort() {
-        assertTrue(true);
+        Integer[] input = new Integer[]{};
+        Integer[] sorted = RadixSort.sort(input);
+
+        assertEquals(0, sorted.length);
     }
 }
